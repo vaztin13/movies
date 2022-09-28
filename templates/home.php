0465ed412239fc4c6bee5f0030fc3c4b5b5417c8
vaztin13 <martintandil13@hotmail.com>
2022-09-28 04:31:57 -0300

    added first sql dump

--- templates/home.php
+++ templates/home.php
@@ -29,22 +29,21 @@ function showHome() {
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                                 aria-expanded="false">
                                 Generos
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <li><a class="dropdown-item" href="#">Accion</a></li>
+                                <li><a class="dropdown-item" href="#">Romance</a></li>
+                                <li><a class="dropdown-item" href="#">Comedia</a></li>
+                                <li><a class="dropdown-item" href="#">Terror</a></li>
+                                <li><a class="dropdown-item" href="#">Ciencia ficcion</a></li>
                             </ul>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Login</a>
                         </li>
                     </ul>
                 </div>
