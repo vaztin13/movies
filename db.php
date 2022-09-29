557000279249255577377be750c3eb3b1d0b0d65
vaztin13 <martintandil13@hotmail.com>
2022-09-29 04:13:16 -0300

    Added viewMovie and getMovies functions based on id's

--- db.php
+++ db.php
@@ -1,14 +1,129 @@
 <?php
 
+function connectToDB(){
+    return new PDO('mysql:host=localhost;' . 'dbname=movie;charset=utf8', 'root', '');
+}
+
+
 
 function getMovies() {
+    $db = connectToDB();
+
+    $sentence = $db->prepare("SELECT * from movies");
+    $sentence->execute();
+    $movies = $sentence->fetchAll(PDO::FETCH_OBJ);
+    return $movies;
+}
+
+function getMovie($id) {
+    $db = connectToDB();
+
+    $sentence = $db->prepare("SELECT * from movies WHERE id = ?");
+    $sentence->execute(array($id));
+    $movie = $sentence->fetch(PDO::FETCH_OBJ);
+    return $movie;
+}
+
+function addMovie($title, $synopsis, $year, $director){
+    $db = connectToDB();
+    $sentencia = $db->prepare("INSERT INTO movies(title, synopsis, year, director) VALUES(?, ?, ?, ?)");
+    $sentencia->execute(array($title,$synopsis,$year, $director));
+}
+
+function deleteMovieFromDB($id) {
+    $db = connectToDB();
+    $sentence = $db->prepare("DELETE FROM movies WHERE id = ? ");
+    $sentence->execute(array($id));
+}
+
+
+function showActionMovie() {
     $db = new PDO('mysql:host=localhost;' . 'dbname=movie;charset=utf8', 'root', '');
 
-    $sentencia = $db->prepare("select * from movies");
-    $sentencia->execute();
-    $movies = $sentencia->fetchAll(PDO::FETCH_OBJ);
+    $sentence = $db->prepare("SELECT * from movies WHERE genre_type_id = 5");
+    $sentence->execute();
+    $movies = $sentence->fetchAll(PDO::FETCH_OBJ);
     foreach($movies as $movie) {
         echo $movie->title;
+        echo $movie->image;
+        echo $movie->synopsis;
+        echo $movie->year;
+        echo $movie->director;
     }
-
 }
+
+function showRomanceMovie() {
+    $db = new PDO('mysql:host=localhost;' . 'dbname=movie;charset=utf8', 'root', '');
+
+    $sentence = $db->prepare("SELECT * from movies WHERE genre_type_id = 2");
+    $sentence->execute();
+    $movies = $sentence->fetchAll(PDO::FETCH_OBJ);
+    foreach($movies as $movie) {
+        echo $movie->title;
+        echo $movie->image;
+        echo $movie->synopsis;
+        echo $movie->year;
+        echo $movie->director;
+    }
+}
+
+function showComedyMovie() {
+    $db = new PDO('mysql:host=localhost;' . 'dbname=movie;charset=utf8', 'root', '');
+
+    $sentence = $db->prepare("SELECT * from movies WHERE genre_type_id = 3");
+    $sentence->execute();
+    $movies = $sentence->fetchAll(PDO::FETCH_OBJ);
+    foreach($movies as $movie) {
+        echo $movie->title;
+        echo $movie->image;
+        echo $movie->synopsis;
+        echo $movie->year;
+        echo $movie->director;
+    }
+}
+
+function showHorrorMovie() {
+    $db = new PDO('mysql:host=localhost;' . 'dbname=movie;charset=utf8', 'root', '');
+
+    $sentence = $db->prepare("SELECT * from movies WHERE genre_type_id = 6");
+    $sentence->execute();
+    $movies = $sentence->fetchAll(PDO::FETCH_OBJ);
+    foreach($movies as $movie) {
+        echo $movie->title;
+        echo $movie->image;
+        echo $movie->synopsis;
+        echo $movie->year;
+        echo $movie->director;
+    }
+}
+
+function showSfMovie() {
+    $db = new PDO('mysql:host=localhost;' . 'dbname=movie;charset=utf8', 'root', '');
+
+    $sentence = $db->prepare("SELECT * from movies WHERE genre_type_id = 4");
+    $sentence->execute();
+    $movies = $sentence->fetchAll(PDO::FETCH_OBJ);
+    foreach($movies as $movie) {
+        echo $movie->title;
+        echo $movie->image;
+        echo $movie->synopsis;
+        echo $movie->year;
+        echo $movie->director;
+    }
+}
+
+function showDramaMovie() {
+    $db = new PDO('mysql:host=localhost;' . 'dbname=movie;charset=utf8', 'root', '');
+
+    $sentence = $db->prepare("SELECT * from movies WHERE genre_type_id = 7");
+    $sentence->execute();
+    $movies = $sentence->fetchAll(PDO::FETCH_OBJ);
+    foreach($movies as $movie) {
+        echo $movie->title;
+        echo $movie->image;
+        echo $movie->synopsis;
+        echo $movie->year;
+        echo $movie->director;
+    }
+}
+
--- route.php
+++ route.php
@@ -3,7 +3,7 @@
 require_once "db.php";
 require_once "templates/home.php";
 
-define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
 
 if (!empty($_GET['action'])) {
     $action = $_GET['action'];
@@ -17,7 +17,34 @@ switch ($params[0]) {
     case 'home':
         showHome();
         break;
+    case 'addMovieToDB':
+        addMovieToDB();
+        break;
+    case 'deleteMovie':
+        deleteMovie($params[1]);
+        break;
+    case 'viewMovie':
+        viewMovie($params[1]);
+        break;
+    case 'action':
+        showActionMovie();
+        break;
+    case 'romance':
+        showRomanceMovie();
+        break;
+    case 'comedy':
+        showComedyMovie();
+        break;
+    case 'horror':
+        showHorrorMovie();
+        break;
+    case 'science-fiction':
+        showSfMovie();
+        break;
+    case 'drama':
+        showDramaMovie();
+        break;
     default:
-        echo('404 page not found');
-        break;    
+        echo ('404 page not found');
+        break;
 }
